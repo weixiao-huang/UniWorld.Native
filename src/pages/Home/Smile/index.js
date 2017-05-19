32226d3405323270ea46b9584d5f2bfef606b8f1
Songsirui <apple@AppledeMacBook-Pro-102.local>
2017-05-19 21:04:25 +0800

    add pictures

--- src/pages/Home/Smile/index.js
+++ src/pages/Home/Smile/index.js
@@ -3,20 +3,44 @@
  */
 
 import React, { Component } from 'react';
-import { Image, View, Button, StyleSheet } from 'react-native'
+import { Image, View, Button, StyleSheet, Text, TouchableOpacity } from 'react-native'
 import styles from '../../../common/styles'
 import Tester from '../../../components/Tester'
 
 export default class Smile extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      number: 0
+    }
+  }
+
+  onPress = () => {
+    
+    this.setState((prevState) => {
+      return { number: prevState.number + 1 } // 上一个 setState 的返回是 count 为 0，当前返回 1
+    })
+    console.log(111)
+    this.setState((prevState) => {
+      return { number: prevState.number + 1 } // 上一个 setState 的返回是 count 为 0，当前返回 1
+    })
+    console.log(222)
+  }
   render() {
     return (
       <View style={[styles.flex1, styles.flexCenter]}>
         {/*<Tester/>*/}
         {/*<Button*/}
-          {/*onPress={() => this.props.navigation.goBack()}*/}
-          {/*title="Go back home"*/}
+        {/*onPress={() => this.props.navigation.goBack()}*/}
+        {/*title="Go back home"*/}
         {/*/>*/}
-        <Image style={[localStyles.cover]} source={require('../../../assets/emptyList.png')}/>
+        <View>
+          <Text>{this.state.number}</Text>
+          <TouchableOpacity onPress={this.onPress}>
+            <Text>点我加2</Text>
+          </TouchableOpacity>
+        </View>
+        <Image style={[localStyles.cover]} source={require('../../../assets/emptyList.png')} />
       </View>
     );
   }
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,122 @@
+/**
+ * Created by huangwx on 10/04/2017.
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, TouchableOpacity, Text, Modal, Platform } from 'react-native'
+import { connect } from 'react-redux'
+import I18n from 'react-native-i18n'
+import styles from '../../common/styles'
+import {
+  Visit, UserLogin, FetchRoomList, FetchWorldRoomList, FetchRecommendRoomList,
+  FetchLatestRoomList, GoToHome, FetchUserInfo, SetCommonData, FetchInitialLabels
+} from '../../store/actions'
+
+import Input from '../Login/Input'
+import LoginButton from '../../components/StyleButton'
+import BackgroundImage from '../../components/BackgroundImage'
+
+import Loading from '../../components/Loading'
+
+const mapStateToProps = state => ({
+  loading: state.common.loading
+})
+
+@connect(mapStateToProps, dispatch=>({dispatch}))
+export default class SignUp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+      email:'',
+    }
+  }
+
+  visit = async () => {
+    // this.props.dispatch(Visit)
+  }
+
+  signup = async () => {
+
+  }
+
+  render () {
+    return (
+      <View style={{flex: 1}}>
+        <Loading visible={this.props.loading}/>
+        <BackgroundImage bgUrl={require('../../assets/background.jpg')}>
+          <View style={loginStyles.container}>
+            <Image
+              source={require('../../assets/Logo.png')}
+              style={loginStyles.logo}
+            />
+            <Input
+              placeholder={I18n.t('Login.username')}
+              icon={require('../../assets/icon/UserIcon.png')}
+              onChangeText={text => this.setState({username: text})}
+              keyboardType="numeric"
+            />
+            <Input
+              placeholder={I18n.t('Login.password')}
+              icon={require('../../assets/icon/email.png')}
+              onChangeText={text => this.setState({password: text})}
+              secureTextEntry={true}
+            />
+            <Input
+              placeholder={I18n.t('Login.password')}
+              icon={require('../../assets/icon/PasswordIcon.png')}
+              onChangeText={text => this.setState({password: text})}
+              secureTextEntry={true}
+            />
+            <LoginButton
+              title={I18n.t('Login.login')}
+              onPress={this.login}
+              inlineStyle={loginStyles.loginButton}
+            />
+            <View style={{flexDirection: 'row'}}>
+              <View style={loginStyles.otherView}>
+                <TouchableOpacity style={loginStyles.otherButton} onPress={this.visit}>
+                  <Text style={{color: 'white'}}>{I18n.t('Login.visitor')}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={loginStyles.otherButton} onPress={this.signup}>
+                  <Text style={{color: 'white'}}>{I18n.t('Login.signup')}</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </BackgroundImage>
+      </View>
+    )
+  }
+}
+
+
+const loginStyles = StyleSheet.create({
+  loginButton: {
+    marginTop: 20,
+    marginBottom: 20,
+    padding: 15,
+  },
+  otherButton: {
+    backgroundColor: 'transparent',
+  },
+  otherView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flex: 1,
+  },
+  logo: {
+    width: 130,
+    height: 130,
+    marginBottom: 60
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    marginLeft: 50,
+    marginRight: 50,
+  }
+})
