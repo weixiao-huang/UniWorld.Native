a44418ac9a71b55dce3bfc0655147415bb4ab179
huangwx <hwx.simle@gmail.com>
2017-05-26 22:46:33 +0800

    重构

--- android/app/src/main/java/com/uniworld/MainApplication.java
+++ android/app/src/main/java/com/uniworld/MainApplication.java
@@ -3,17 +3,17 @@ package com.uniworld;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.beefe.picker.PickerViewPackage;
 import com.imagepicker.ImagePickerPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.beefe.picker.PickerViewPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,11 +30,12 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new PickerPackage(),
             new RNSpinkitPackage(),
-            new VectorIconsPackage(),
-            new PickerViewPackage(),
             new ImagePickerPackage(),
+            new VectorIconsPackage(),
+            new ReactNativePushNotificationPackage(),
+            new PickerViewPackage(),
+            new PickerPackage(),
             new ReactNativeI18n()
       );
     }
@@ -50,17 +51,4 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
   }
-
-    
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    protected boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(new MainReactPackage(), new ReactNativePushNotificationPackage());
-    }
-  };
 }
