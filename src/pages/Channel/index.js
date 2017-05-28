800169c572466c7019ba4ec93b1954fa7f61031b
Songsirui <apple@AppledeMacBook-Pro-102.local>
2017-05-29 03:11:50 +0800

    频道写好了

--- src/pages/Channel/index.js
+++ src/pages/Channel/index.js
@@ -3,37 +3,91 @@ import { StyleSheet, ScrollView, View, Image, TouchableOpacity, Text, Modal, Pla
 import { connect } from 'react-redux'
 import I18n from 'react-native-i18n'
 import styles from '../../common/styles'
-import {
-} from '../../store/actions'
-
+import api from '../../api'
+import { server } from '../../common/constants'
 
 import RoomWrap from '../../components/RoomWrap'
 
 import Loading from '../../components/Loading'
 
 const mapStateToProps = state => ({
-  channels: state.room.channels
+    token: state.auth.token
 })
 
 @connect(mapStateToProps, dispatch => ({dispatch}))
 export default class Input extends Component {
   constructor(props){
     super(props)
-    let channel = this.props.channels[global.channelId]
     this.state={
-      cover: channel.cover,
+      id: global.channelId,
+      searchResult: []
     }
   }
 
+  componentWillMount = async () => {
+    this.setState({searchResult: []})
+    try {
+      const res = await api.fetchDataFromUrl(`${server}/channel/${global.channelId}/`)(this.props.token)
+      console.log(res)
+      if (res.status === 200) {
+        const data = await res.json()
+        console.log(data)
+        if (data.rooms && data.rooms.length > 0) {
+          this.setState({
+            searchResult: data.rooms,
+            next: data.next,
+            cover: data.cover
+          })
+        }
+        this.setState({name: ''})
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
+   _showNext = async () => {
+    this.setState({isFetching: true})
+    try {
+      const res = await api.fetchDataFromUrl(this.state.next)(this.props.token)
+      console.log(res)
+      if (res.status === 200) {
+        const data = await res.json()
+        this.setState({searchResult: this.state.searchResult.concat(data.results), next: data.next})
+      }
+    } catch (e) {
+      console.log(e)
+    }
+    this.setState({isFetching: false})
+  }
 
 
   render(){
     return(
-      <ScrollView style={{backgroundColor: 'white'}}>
+      <ScrollView style={[styles.flex1, localStyles.container]}>
         <View style={{height: 20, backgroundColor: '#ec5367', width: '100%'}}/>
         <Image style={[localStyles.smallPoster]} source={{uri: this.state.cover}} />
-        {/*<RoomWrap titleLabel="HOT" roomList={this.props.world.results} />*/}
+        { this.state.searchResult.length > 0 ?
+          <RoomWrap roomList={this.state.searchResult} />
+          :
+          <View style={[styles.flex1, styles.flexCenter]}>
+            {/*<Image style={[localStyles.cover]} source={require('../../assets/emptyList.png')}/>*/}
+          </View>
+        }
+        {this.state.isFetching ?
+          <ActivityIndicator
+            style={{ height: 40 }}
+            animating={this.state.isFetching}
+          /> :
+          this.state.next && <TouchableOpacity>
+            <Text
+              style={[localStyles.next]}
+              onPress={this._showNext}
+            >
+              {I18n.t('RoomList.searchNext')}
+            </Text>
+          </TouchableOpacity>
+        }
       </ScrollView>
     )
   }
@@ -48,4 +102,33 @@ const localStyles = StyleSheet.create({
     width: 345,
     borderRadius: 16,
   },
+
+  container: {
+    backgroundColor: 'white'
+  },
+  cover: {
+    resizeMode: 'contain',
+    width: '70%',
+  },
+  next: {
+    textAlign: 'center',
+    color: '#aaa',
+    padding: 10
+  },
+  inputBox: {
+    padding: 5,
+    flexDirection: 'row',
+    height: 50,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee'
+  },
+  input: {
+    backgroundColor: '#eee',
+    paddingLeft: 10
+  },
+  button: {
+    marginLeft: 15,
+    marginRight: 15,
+    color: '#6485ed'
+  }
 })
