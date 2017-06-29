1382238fec580a0fb0c7ce8a04b4b4af22f09c75
ZXCroon <zxcroon@gmail.com>
2017-06-30 00:11:48 +0800

    fix height

--- src/pages/login/components/Input/style.js
+++ src/pages/login/components/Input/style.js
@@ -2,18 +2,16 @@ import styled from 'styled-components/native'
 
 export const MainView = styled.View`
   flex-direction: row;
-  margin-top: 10px;
   margin-bottom: 10px;
   padding: 10px;
+  padding-top: 0;
+  padding-bottom: 0;
   background-color: white;
   border-radius: 5px;
   align-items: center;
 `
 
 export const MainInput = styled.TextInput`
-  height: 30px;
-  top: 5px;
-  padding-top: 5px;
   flex: 1;
   padding-left: 10px;
 `
