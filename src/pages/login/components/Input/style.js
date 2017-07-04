5ba5a8b895c9904a9269dbdab746becac1dff88b
huangwx <hwx.simle@gmail.com>
2017-07-04 22:51:21 +0800

    improve the style written by czx of login input

--- src/pages/login/components/Input/style.js
+++ src/pages/login/components/Input/style.js
@@ -1,16 +1,10 @@
 import styled from 'styled-components/native'
 import { Platform } from 'react-native'
 
-const padding = Platform.OS === 'ios' ? '' : `
-  padding-top: 0;
-  padding-bottom: 0;
-`
-
 export const MainView = styled.View`
   flex-direction: row;
   margin-bottom: 10px;
-  padding: 10px;
-  ${ padding }
+  padding: ${Platform.OS === 'ios' ? '10px' : '0'} 10px;
   background-color: white;
   border-radius: 5px;
   align-items: center;
