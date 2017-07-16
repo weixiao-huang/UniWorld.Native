5f0b1f67eebc3992c249f570ff38144d20a3cc94
Songsirui <apple@AppledeMacBook-Pro-102.local>
2017-07-16 16:08:30 +0800

    给登录过期增加了提示框；把lanuchImage调好了

--- src/lib/api-errors.js
+++ src/lib/api-errors.js
@@ -2,13 +2,26 @@
 // actual errors since it's technically completing the response...
 
 import Reactotron from 'reactotron-react-native'
+import { Alert } from 'react-native'
+import I18n from '@/locales'
+// import { logout } from '@/pages/login/sagas'
 
-export const handleApiErrors = (response) => {
+export function handleApiErrors(response) {
   if (!response.ok) {
     Reactotron.log(response.statusText)
     console.log('response', response)
     if (response.status === 401) {
-      // 登录过期
+      console.log('expire')
+      Alert.alert(
+        I18n.t('Alert.tips'),
+        I18n.t('Alert.tokenExpired'),
+        [
+          {
+            text: 'OK',
+            onPress: () => {},
+          },
+        ],
+      )
     }
     throw new Error(response.status)
   }
